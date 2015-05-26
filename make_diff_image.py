f8bf34c730e61a8617b1a0517b72fa2680cce2fc
Jae-Joon Lee <lee.j.joon@gmail.com>
2015-05-26 17:11:06 +0900

    use specific prefix for temp dir names.

--- make_diff_image.py
+++ make_diff_image.py
@@ -15,43 +15,61 @@ Make a difference image , given source image and reference image.
 
 from ksppy.hotpants import tempfile, run_wcsremap, run_hotpants
 
-def make_diff_image(src_name, ref_name, out_name):
-    tmpdir = tempfile.mkdtemp(dir=".")
-    print tmpdir
+TMP_PREFIX = "tmp_make_diff_image_"
 
-    #catname = run_sex(src_name, tmpdir=tmpdir)
-    #catname = "tmp/uwife040_FeII_S0_1v0_x_starsub.cat"
+def make_diff_image(src_name, ref_name, out_name, delete_temp=True):
+    try:
+        tmpdir = tempfile.mkdtemp(dir=".", prefix=TMP_PREFIX+src_name+"_")
+        print tmpdir
 
-    #headername = run_scamp(catname, tmpdir=tmpdir)
-    #headername = "tmp/uwife040_FeII_S0_1v0_x_starsub.head"
+        #catname = run_sex(src_name, tmpdir=tmpdir)
+        #catname = "tmp/uwife040_FeII_S0_1v0_x_starsub.cat"
 
-    #src_templatename = update_header(src_name, headername,
-    # outdir=tmpdir, prefix="nh.fits")
+        #headername = run_scamp(catname, tmpdir=tmpdir)
+        #headername = "tmp/uwife040_FeII_S0_1v0_x_starsub.head"
 
-    #src_templatename = "tmp/uwife040_FeII_S0_1v0_x_starsub.nh.fits"
+        #src_templatename = update_header(src_name, headername,
+        # outdir=tmpdir, prefix="nh.fits")
 
-    remaped_ref = run_wcsremap(ref_name, src_name, outdir=tmpdir)
-    #remaped_ref = "tmp/uwife040_H_S0_1v0_x_starsub.ref.remap.fits"
+        #src_templatename = "tmp/uwife040_FeII_S0_1v0_x_starsub.nh.fits"
 
-    run_hotpants(src_name,
-                 templatename=remaped_ref,
-                 outname=out_name)
+        remaped_ref = run_wcsremap(ref_name, src_name, outdir=tmpdir)
+        #remaped_ref = "tmp/uwife040_H_S0_1v0_x_starsub.ref.remap.fits"
+
+        run_hotpants(src_name,
+                     templatename=remaped_ref,
+                     outname=out_name)
+
+    finally:
+        if delete_temp:
+            import shutil
+            shutil.rmtree(tmpdir)
 
 
 if __name__ == "__main__":
-    import sys
 
-    assert len(sys.argv) == 4
+    import argparse
 
-    src_name = sys.argv[1]
-    ref_name = sys.argv[2]
-    out_name = sys.argv[3]
+    parser = argparse.ArgumentParser(description='make diff images.')
+    parser.add_argument('src_name', type=str, help='input file name')
+    parser.add_argument('ref_name', type=str, help='reference image')
+    parser.add_argument('out_name', type=str, help='out name')
+    parser.add_argument('--debug', dest='debug', action='store_const',
+                        const=True, default=False,
+                        help='do not delete temp files')
+
+    args = parser.parse_args()
+
+    #print args.debug
+    #print args.ref_name, args.tgt_name
+    delete_temp = not args.debug
 
     if 0: # for test
         src_name = "uwife040_FeII_S0_1v0_x_starsub.fits"
         ref_name = "uwife040_H_S0_1v0_x_starsub.ref.fits"
         out_name = "starsub.fits"
 
-    make_diff_image(src_name, ref_name, out_name)
+    make_diff_image(args.src_name, args.ref_name, args.out_name,
+                    delete_temp=delete_temp)
 
     #make_ref_image(ref_name, out_name)
--- update_wcs.py
+++ update_wcs.py
@@ -1,34 +1,60 @@
 from ksppy.hotpants import (tempfile, run_sex, run_scamp, update_header,
                             run_swarp)
+TMP_PREFIX = "tmp_update_wcs_"
 
-def update_wcs(src_name):
-    tmpdir = tempfile.mkdtemp(dir=".")
-    print tmpdir
+def update_wcs(src_name, delete_temp=False):
+    try:
+        tmpdir = tempfile.mkdtemp(dir=".", prefix=TMP_PREFIX+src_name+"_")
+        print tmpdir
 
-    resamp_name = run_swarp(src_name, tmpdir=tmpdir)
-    catname = run_sex(resamp_name, tmpdir=tmpdir)
-    #catname = "tmp/uwife040_FeII_S0_1v0_x_starsub.cat"
+        #src_name0, ext0 = os.path.splitext(src_name)
+        if src_name.endswith(".fz"):
+            import os
+            basename = os.path.basename(src_name.replace(".fz", ""))
+            src_name0 = os.path.join(tmpdir,basename)
+            os.system("funpack -O %s %s" % (src_name0, src_name))
+        else:
+            src_name0 = src_name
 
-    headername = run_scamp(catname, tmpdir=tmpdir)
-    #headername = "tmp/uwife040_FeII_S0_1v0_x_starsub.head"
+        resamp_name = run_swarp(src_name0, tmpdir=tmpdir)
+        catname = run_sex(resamp_name, tmpdir=tmpdir)
+        #catname = "tmp/uwife040_FeII_S0_1v0_x_starsub.cat"
 
-    #src_name = "kmtc.20150211.003540.ne.fits"
-    #headername = "tmpD5iOJ_/test.ne.head"
-    src_templatename = update_header(resamp_name, headername,
-                                     outdir=".", prefix="nh.fits", extnum=0)
+        headername = run_scamp(catname, tmpdir=tmpdir)
+        #headername = "tmp/uwife040_FeII_S0_1v0_x_starsub.head"
 
-    print src_templatename
+        #src_name = "kmtc.20150211.003540.ne.fits"
+        #headername = "tmpD5iOJ_/test.ne.head"
+        src_templatename = update_header(resamp_name, headername,
+                                         outdir=".", prefix="nh.fits", extnum=0)
 
+        print src_templatename
+
+    finally:
+        if delete_temp:
+            import shutil
+            shutil.rmtree(tmpdir)
 
 
 if __name__ == "__main__":
-    import sys
 
-    assert len(sys.argv) == 2
+    import argparse
 
-    src_name = sys.argv[1]
+    parser = argparse.ArgumentParser(description='update wcs.')
+    parser.add_argument('src_name', type=str, help='input file name')
+    #parser.add_argument('tgt_name', type=str, help='target file name')
+    parser.add_argument('--debug', dest='debug', action='store_const',
+                        const=True, default=False,
+                        help='do not delete temp files')
 
+    args = parser.parse_args()
 
-    update_wcs(src_name)
+    #print args.debug
+    #print args.ref_name, args.tgt_name
+    delete_temp = not args.debug
+
+    print delete_temp
+
+    update_wcs(args.src_name, delete_temp=delete_temp)
 
     #make_ref_image(ref_name, out_name)
