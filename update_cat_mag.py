e72c2f374bac4391e4ecf997a521d87fce42254a
Jae-Joon Lee <lee.j.joon@gmail.com>
2015-10-25 22:16:35 +0900

    update_cat_mag.py: update description of parameter

--- update_cat_mag.py
+++ update_cat_mag.py
@@ -4,7 +4,7 @@ from ksppy.phot import update_cat_mag
 def main():
     import argparse
 
-    parser = argparse.ArgumentParser(description='update wcs.')
+    parser = argparse.ArgumentParser(description='update MAG_AUTO using the given magzpt information')
     parser.add_argument('cat_name', type=str, help='input file name')
     parser.add_argument('magzpt_name', type=str, help='input magzpt name')
     parser.add_argument('outcat_name', type=str, help='output cat name')
