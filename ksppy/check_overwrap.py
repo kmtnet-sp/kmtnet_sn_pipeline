382fe6c9b235922633182c2baab93c4dc306e3d5
Jae-Joon Lee <lee.j.joon@gmail.com>
2017-09-13 08:30:27 +0900

    fix incorrect separation

--- ksppy/check_overwrap.py
+++ ksppy/check_overwrap.py
@@ -1,23 +1,43 @@
 import numpy as np
 import astropy.io.fits as pyfits
 
+from astropy.coordinates import spherical_to_cartesian, cartesian_to_spherical
+from astropy.coordinates import SkyCoord
+
+
+def get_median_lonlat(lon, lat):
+    "lon, lat in degrees"
+    x, y, z = spherical_to_cartesian(1, (lat / 180. * np.pi), (lon / 180. * np.pi))
+    # print(np.median(x))                                                                          
+    # print(np.median(y))                                                                          
+    # print(np.median(z))                                                                          
+
+    r, lat, lon = cartesian_to_spherical(np.median(x), np.median(y), np.median(z))
+    return lon, lat
+
+
 def get_dist(src, ref):
     f = pyfits.open(src)
 
-    xw1 = np.median(f[2].data["X_WORLD"])
-    yw1 = np.median(f[2].data["Y_WORLD"])
+    lon1, lat1 = get_median_lonlat(f[2].data["X_WORLD"], f[2].data["Y_WORLD"])
 
     fc = pyfits.open(ref)
-    xw2 = np.median(fc[-1].data["X_WORLD"])
-    yw2 = np.median(fc[-1].data["Y_WORLD"])
 
-    dx = (xw1 - xw2) * np.cos(0.5*(yw1 + yw2)/180.*np.pi)
-    dy = yw1 - yw2
+    lon2, lat2 = get_median_lonlat(np.median(fc[-1].data["X_WORLD"]), fc[-1].data["Y_WORLD"]))
 
-    d = (dx**2 + dy**2)**.5
-    dist_in_arcmin = d*60
+    c1 = SkyCoord(lon1, lat1)
+    c2 = SkyCoord(lon2, lat2)
 
-    return dist_in_arcmin
+    return c1.separation(c2).arcmin
+
+
+    # dx = (xw1 - xw2) * np.cos(0.5*(yw1 + yw2)/180.*np.pi)
+    # dy = yw1 - yw2
+
+    # d = (dx**2 + dy**2)**.5
+    # dist_in_arcmin = d*60
+
+    # return dist_in_arcmin
 
 
 if __name__ == "__main__":
